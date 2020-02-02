e05e3245c7adc0e1ddf9c415c436707af39d8582
briniac <batuhanonur29@gmail.com>
2020-02-02 18:07:51 +0300

    sign in button and component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from '../src/pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Switch, Route } from 'react-router-dom'
 
 
@@ -19,6 +20,10 @@ function App() {
        <Route        
        path='/shop' 
        component={ShopPage} />
+
+       <Route        
+       path='/signin' 
+       component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
