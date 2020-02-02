4354f744f67af8d96449823ae1fc689199169220
briniac <batuhanonur29@gmail.com>
2020-02-02 22:41:02 +0300

    firebase database updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Switch, Route } from 'react-router-dom'
 
-import { auth } from './firebase/firebase.utils';
+import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -22,9 +22,21 @@ class App extends React.Component {
   unsbscribeFromAuth = null
 
   componentDidMount(){  // uygulamaya giriş yaptığımızda user session'ı tut.
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState( {currentUser: user })
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      }
+        this.setState({ currentUser: userAuth})
+    });
   }
 
   componentWillUnmount(){  // session takibi.
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,35 @@ const config= {
     measurementId: "G-Q3JS0FCFGH"
   };
 
+   // giriş yapan kullanıcının profil bilgilerini oluşturma
+  export const createUserProfileDocument = async(userAuth,additionalData) => {
+
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+    
+    if(!snapShot.exists){
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+
+         await userRef.set({
+             displayName,
+             email,
+             createdAt,
+             ...additionalData
+         })
+
+        }catch(error){
+             console.log('error',error.message);
+        }
+    }
+      return userRef;
+  }
+
   firebase.initializeApp(config);
 
 
