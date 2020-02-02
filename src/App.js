edaf4fe1e352190fa7848ff233ee98f41a108ceb
briniac <batuhanonur29@gmail.com>
2020-02-02 17:20:53 +0300

    adding header comp

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from '../src/pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Switch, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <div>
-
+      <Header />      
       <Switch>
        <Route        //exact olmazsa / ile başlayan tüm sayfalar renderlanır.
        exact        
